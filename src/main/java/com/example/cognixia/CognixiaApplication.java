0c967e3da1d838ccd6ceb1d74ac02ae47747c9f3
zkrem56 <zkremer702@gmail.com>
2022-03-10 09:23:10 -0600

    Initial commit

---
+++ src/main/java/com/example/cognixia/CognixiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cognixia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognixiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognixiaApplication.class, args);
+	}
+
+}
